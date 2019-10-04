98ad2604a4c2b25aadb1b87ad1e12b690faa5986
H-Asakawa <erimingo2718@yahoo.co.jp>
2019-10-04 09:42:16 +0900

    ページのタイトルを修正

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'About me',
+      title: 'Asakawa House',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
